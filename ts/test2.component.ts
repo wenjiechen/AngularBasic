58e5aedaad62206950652dc4043172421daf82f9
wenjiechen <wenjie.chen@nyu.edu>
2017-02-03 16:06:35 -0500

    add test file

---
+++ ts/test2.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by wenchen on 2/3/17.
+ */
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'c1',
+  template: `<h2>second input box</h2><input type="text"/>`
+
+})
+export class C1 {
+  type = '';
+}
